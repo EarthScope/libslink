f6961c907d82dca61b01000c466be6c3a70188ba
chad-iris <chad.trabant@iris.edu>
2020-02-15 16:48:00 -0800

    Convert README to markdown README.md and update license to Apache 2

--- genutils.c
+++ genutils.c
@@ -2,12 +2,22 @@
  *
  * General utility functions.
  *
- * Written by Chad Trabant, ORFEUS/EC-Project MEREDIAN
+ * This file is part of the SeedLink Library.
  *
- * Originally based on the SeedLink interface of the modified Comserv in
- * SeisComP written by Andres Heinloo
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * Version: 2008.028
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright (C) 2020:
+ * @author Chad Trabant, IRIS Data Management Center
  ***************************************************************************/
 
 #include <stdio.h>
--- msrecord.c
+++ msrecord.c
@@ -10,11 +10,22 @@
  * and 1001.  The Blockettes are optionally parsed and the data samples
  * are optionally decompressed/unpacked.
  *
- * Some ideas and structures were used from seedsniff 2.0
+ * This file is part of the SeedLink Library.
  *
- * Written by Chad Trabant, IRIS Data Managment Center
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * modified: 2016.288
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright (C) 2020:
+ * @author Chad Trabant, IRIS Data Management Center
  ***************************************************************************/
 
 #include <stdio.h>
